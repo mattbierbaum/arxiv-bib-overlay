04476032133cf50145aa41c55510034be86ef89d
Matt Bierbaum <matt.bierbaum@gmail.com>
2018-09-07 13:32:11 -0400

    creating semantic scholar data source

---
+++ src/api/S2Datasource.ts
@@ -0,0 +1,118 @@
+import icon from '../assets/icon-s2.png'
+import sourceLogo from '../assets/source-s2.png'
+//import { API_ARTICLE_COUNT } from '../bib_config'
+//import { encodeQueryData } from '../bib_lib'
+import { BasePaper, DataSource, Paper } from './document'
+import { S2ToPaper } from './S2FromJson'
+
+export class S2Datasource implements DataSource {
+    data: BasePaper
+    cache: { [key: string]: Paper } = {}
+    aid: string
+
+    logo = sourceLogo
+    icon = icon
+
+    shortname = 'S2'
+    longname = 'Semantic Scholar'
+    categories = new Set(['cs', 'stat.ML'])
+    homepage = 'https://semanticscholar.org'
+    api_url = 'https://api.semanticscholar.org/v1/'
+    api_params = {include_unknown_references: 'true'}
+
+    sorting = {
+        sorters: {
+            paper: {name: 'Paper order', func: (i) => i.index},
+            influence: {name: 'Influence', func: (i) => i.isInfluential},
+            author: {name: 'First author', func: (i) => i.authors[0] && i.authors[0].tolastname()},
+            title: {name: 'Title', func: (i) => i.title.toLowerCase()},
+            year: {name: 'Year', func: (i) => i.year},
+        },
+        sorters_order: ['influence', 'title', 'author', 'year'],
+        sorters_default: 'influence',
+    }
+
+    json_to_doc = new S2ToPaper(this)
+
+    url_paper(arxivid: string) {
+        return `${this.api_url}paper/arXiv:${arxivid}?${this.api_params}`
+    }
+
+    populate(json: any) {
+        const output: BasePaper = this.json_to_doc.reformat_document(json, 0)
+
+        if (json.citations) {
+            output.citations = {
+                documents: json.citations.map((doc) => this.json_to_doc.reformat_document(doc, 0)),
+                header: 'Citations',
+                header_url: `${json.url}#citingPapers`,
+                description: 'highly influenced citations',
+                count: json.citations.length,
+                sorting: this.sorting,
+            }
+        }
+
+        if (json.references) {
+            output.citations = {
+                documents: json.references.map((doc) => this.json_to_doc.reformat_document(doc, 0)),
+                header: 'References',
+                header_url: `${json.url}#citedPapers`,
+                description: 'highly influenced references',
+                count: json.references.length,
+                sorting: this.sorting,
+            }
+        }
+
+        this.data = output
+    }
+
+        /*async_load: function(callback){
+        this.aid = bib_lib.get_current_article();
+        var url = this.url_paper(this.aid);
+
+        if (url in this.cache){
+            callback(this);
+            return;
+        }
+
+        $.ajax({
+            type: 'GET',
+            url: bib_lib.urlproxy(url),
+            async: true,
+            timeout: bib_config.API_TIMEOUT,
+            success: $.proxy(
+                function(data){
+                   this.data = this.transform_result(data);
+                   this.cache[url] = this.data;
+                   callback(this);
+                }, this
+            ),
+            error: this.query_error,
+        });
+    }
+
+    get_paper: function(url, callback){
+        if (url in this.cache)
+            return callback(this.cache[url]);
+
+        $.get(url, $.proxy(
+            function(data){
+                data = this.transform_result(data);
+                this.cache[url] = data;
+                callback(data);
+            }, this)
+        )
+        .fail(function(err) {});
+    }*/
+
+    fetch_all(arxiv_id: string): Promise<S2Datasource> {
+        this.aid = arxiv_id
+
+        return fetch(this.url_paper(this.aid))
+            .then(resp => resp.json())
+            .then(json => {
+                this.populate(json)
+                return this
+            })
+    }
+}
---
+++ src/api/S2FromJson.ts
@@ -0,0 +1,74 @@
+//import { RE_IDENTIFIER } from '../arxiv_page'
+import { encodeQueryData } from '../bib_lib' 
+import { Author, Paper } from './document'
+import { S2Datasource } from './S2Datasource'
+
+export class S2ToPaper {
+    fetchConfig: S2Datasource
+
+    constructor(fetch_config: S2Datasource) {
+        this.fetchConfig = fetch_config
+    }
+
+    url_author(id: string) {
+        return `${this.fetchConfig.api_url}author/${id}`
+    }
+
+    url_api(json: any) {
+        const params = encodeQueryData(this.fetchConfig.api_params)
+        return json.paperId ? `${this.fetchConfig.api_url}paper/${json.paperId}?${params}` : ''
+    }
+
+    url_arxiv(json: any) {
+        return json.arxivId ? `https://arxiv.org/abs/${json.arxivId}` : ''
+    }
+
+    url_doi(json: any) {
+        return json.doi ? `https://doi.org/${json.doi}` : ''
+    }
+
+    searchline(doc: Paper) {
+        return [doc.title, doc.venue, doc.year].join(' ').toLowerCase()
+    }
+
+    outbound_names(ref: Paper) {
+        const outs = [this.fetchConfig.shortname.toLocaleLowerCase()]
+        if (ref.url_arxiv) { outs.push('arxiv') }
+        if (ref.url_doi) { outs.push('doi') }
+        outs.push('scholar')
+        return outs
+    }
+
+    doc_authors(json: any): Author[] {
+        const toauth = (item) => {
+            const auth = new Author()
+            auth.name = item.name
+            auth.url = item.url
+            return auth
+        }
+        return json.authors.map(toauth)
+    }
+
+    reformat_document(json: any, index: number) {
+        const newdoc: Paper = new Paper(json.arxivId)
+
+        newdoc.title = json.title
+        newdoc.year = json.year
+        newdoc.venue = json.venue
+        newdoc.citation_count = json.citation_count
+        newdoc.url = json.url
+        newdoc.doi = json.doi
+        newdoc.arxivId = json.arxivId
+
+        newdoc.authors = this.doc_authors(json)
+        newdoc.api = this.url_api(json)
+        newdoc.url_arxiv = this.url_arxiv(json)
+        newdoc.url_doi = this.url_doi(json)
+
+        newdoc.paperId = json.paperId
+
+        newdoc.searchline = this.searchline(newdoc)
+        newdoc.outbound = this.outbound_names(newdoc)
+        return newdoc
+    }
+}
