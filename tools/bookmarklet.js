91ca9c0c9de75b0ed1fbfd32046cccd124ae6bd9
Matt Bierbaum <matt.bierbaum@gmail.com>
2018-09-13 10:41:32 -0400

    python bookmarklet test server

---
+++ tools/bookmarklet.js
@@ -0,0 +1,55 @@
+(
+    function(){
+        function random_id(){
+            return String(Math.random()).substring(2,12);
+        }
+
+        var date = random_id();
+        var base = 'http://127.0.0.1:8000/static/';
+
+        function load(filename, callback){
+            /*
+             This is a bit complicated in order to have deferred, ordered
+             processing while maintaining parallel downloading. This should
+             cover all browsers down to:
+                  FF 3.6, Chrome 8, Opera 15, Safari 5.1, Android 3, IE 6
+
+              but specifically not for Opera Mini (2.5% of all browsing)
+            */
+            var elem = null;
+
+            if (/\.css[^\.]*$/.test(filename)){
+                elem = document.createElement('link');
+                elem.rel  = 'stylesheet';
+                elem.type = 'text/css';
+                elem.href = base + filename + '?' + date;
+                elem.media = 'all';
+            } else {
+                elem = document.createElement('script');
+                elem.type = 'text/javascript';
+                elem.src = base + filename + '?' + date;
+            }
+
+            elem.defer = false;
+            elem.async = false;
+            elem.onload = elem.error = elem.onreadystatechange = function(event){
+                var ie678 = (
+                    /loaded|complete/.test(elem.readyState) &&
+                    (!doc.documentMode || doc.documentMode < 9)
+                );
+                if (event.type === "load" || ie678){
+                    elem.onload = elem.onreadystatechange = elem.onerror = null;
+                }
+                if (callback)
+                    callback();
+            }
+
+            // see comments in headjs/load.js:475 (loadAsset)
+            // or https://www.html5rocks.com/en/tutorials/speed/script-loading/
+            var head = document.head || doc.getElementsByTagName('head')[0];
+            head.insertBefore(elem, head.lastChild);
+        }
+
+        var filenames = @{filenames}.forEach(function(i){load.apply(this, i);});
+    }
+)();
--- tools/server.py
+++ tools/server.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python
-try:
-    # Python 3
-    from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
-    import sys
-    def test (*args):
-        test_orig(*args, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
-except ImportError: # Python 2
-    from BaseHTTPServer import HTTPServer, test
-    from SimpleHTTPServer import SimpleHTTPRequestHandler
+import os
+import json
+import string
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
+build_path = os.path.join(os.path.abspath(os.path.curdir), 'build')
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+class AtTemplate(string.Template):
+    delimiter = '@'
+
+def create_bookmarklet():
+    template = os.path.join(script_path, 'bookmarklet.js')
+    manifest = os.path.join(build_path, 'asset-manifest.json')
+    return bookmarklet_text(template, manifest)
+
+def bookmarklet_text(template, manifest):
+    filenames = list(json.load(open(manifest)).values())
+
+    tpl = AtTemplate(open(template).read())
+    return tpl.substitute(filenames=filenames)
+
+class FlexibleBookmarklet(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path != '/bookmarklet.js':
+            return super(FlexibleBookmarklet, self).do_GET()
+
+        content = create_bookmarklet()
+        self.send_response(200)
+        self.send_header('Content-type', 'text/javascript')
+        self.end_headers()
+        self.wfile.write(content.encode('utf-8'))
+
+    def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer)
+    test(FlexibleBookmarklet, HTTPServer, port=8001)
