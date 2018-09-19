bcd8d3aba8d428637bfb94d226ccc53d41e576a0
Matt Bierbaum <matt.bierbaum@gmail.com>
2018-09-19 12:19:12 -0400

    preparing for extension

---
+++ tools/packextension.py
@@ -0,0 +1,16 @@
+import os
+import glob
+import shutil
+
+j = os.path.join
+
+def find(path, st):
+    paths = glob.glob(j(path, st))
+    if paths:
+        return paths[0]
+
+p0 = os.path.abspath('./build/static')
+p1 = os.path.abspath('./extension')
+
+shutil.copyfile(find(p0, 'js/main.*.js'), j(p1, 'js/main.js'))
+shutil.copyfile(find(p0, 'css/main.*.css'), j(p1, 'css/main.css'))
