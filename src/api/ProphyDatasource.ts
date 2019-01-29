b5cc3b48d4772cee2935353a99805e78cdc3ce31
Matt Bierbaum <matt.bierbaum@gmail.com>
2019-01-29 11:50:01 -0500

    adding basics of prophy data source

---
+++ src/api/ProphyDatasource.ts
@@ -0,0 +1,122 @@
+import icon from '../assets/icon-prophy.png'
+import sourceLogo from '../assets/source-prophy.png'
+import { encodeQueryData } from '../bib_lib'
+import { api_bucket } from '../leaky_bucket'
+import { BasePaper, DataSource, DOWN, QueryError, UP } from './document'
+import { ProphyToPaper } from './ProphyFromJson'
+
+export class ProphyDatasource implements DataSource {
+    data: BasePaper
+    loaded: boolean = false
+    aid: string
+
+    logo = sourceLogo
+    icon = icon
+
+    max_count = 999
+    email = 'feedback@www.prophy.science'
+    shortname = 'Prophy'
+    longname = 'Prophy'
+    categories = new Set(['math', 'cond-mat'])
+    homepage = 'https://www.prophy.science'
+    api_url = 'https://www.prophy.science/api'
+    api_params = {include_unknown_references: 1}
+
+    sorting = {
+        sorters: {
+            author: {name: 'First author', func: (i) => i.authors[0] && i.authors[0].tolastname().toLowerCase()},
+            title: {name: 'Title', func: (i) => i.simpletitle},
+            year: {name: 'Year', func: (i) => i.year},
+        },
+        sorters_order: ['title', 'author', 'year'],
+        sorters_updown: {
+            title: UP,
+            author: UP,
+            year: DOWN
+        },
+        sorters_default: 'year',
+    }
+
+    json_to_doc = new ProphyToPaper(this)
+
+    url_paper(arxivid: string) {
+        const params = encodeQueryData(this.api_params)
+        return `${this.api_url}/arxiv/${arxivid}?${params}`
+    }
+
+    populate(json: any) {
+        const output: BasePaper = this.json_to_doc.reformat_document(json, 0)
+
+        if (json.citations) {
+            output.citations = {
+                documents: json.citations.map((doc, index) => this.json_to_doc.reformat_document(doc, index)),
+                header: 'Citations',
+                header_url: `${json.url}#citingPapers`,
+                description: 'highly influenced citations',
+                count: json.citations.length,
+                sorting: this.sorting,
+            }
+        }
+
+        if (json.references) {
+            output.references = {
+                documents: json.references.map((doc, index) => this.json_to_doc.reformat_document(doc, index)),
+                header: 'References',
+                header_url: `${json.url}#citedPapers`,
+                description: 'highly influenced references',
+                count: json.references.length,
+                sorting: this.sorting,
+            }
+        }
+
+        this.data = output
+    }
+
+    fetch_all(arxiv_id: string): Promise<ProphyDatasource> {
+        if (this.loaded) {
+            return new Promise<ProphyDatasource>((resolve, reject) => resolve(this))
+        }
+
+        this.aid = arxiv_id
+
+        return api_bucket.throttle(
+            () => fetch(this.url_paper(this.aid))
+                .catch((e) => {
+                    console.log(e)
+                    throw new QueryError('Query prevented by browser -- CORS, firewall, or unknown error')}
+                )
+                .then(resp => error_check(resp))
+                .then(resp => resp.json())
+                .then(json => {
+                    console.log(json) 
+                    this.populate(json)
+                    this.loaded = true
+                    return this
+                })
+        ).catch((e) => {
+            if (e instanceof QueryError) {
+                throw e
+            } else {
+                throw new Error('Too many requests, please try again in a few seconds.')
+            }
+        })
+    }
+}
+
+function error_check(response: Response) {
+    if (response.status === 200) {
+        return response
+    }
+
+    switch (response.status) {
+        case 0:
+            console.log(response)
+            throw new QueryError('Query prevented by browser -- CORS, firewall, or unknown error')
+        case 404:
+            throw new QueryError('No data available yet')
+        case 500:
+            throw new QueryError('Query error 500: internal server error')
+        default:
+            throw new QueryError('Query error ' + response.status)
+    }
+}
---
+++ src/api/ProphyFromJson.ts
@@ -0,0 +1,94 @@
+//import { RE_IDENTIFIER } from '../arxiv_page'
+import * as unidecode from 'unidecode'
+import { encodeQueryData, remove_puctuation } from '../bib_lib'
+import { Author, Paper } from './document'
+import { ProphyDatasource } from './ProphyDatasource'
+
+function remove_title_dates(title: string): string {
+    // Remove things from Prophy titles that look like:
+    //      1 1 A pr 2 01 8
+    //      1 S ep 2 01 6
+    //      2 5 O ct 2 01 6
+    return title.replace(/^(?:\d\s*){1,2}(?:[A-Za-z]\s*){2,4}\s*(?:\d\s*){4,}/, '')
+}
+
+export class ProphyToPaper {
+    fetchConfig: ProphyDatasource
+
+    constructor(fetch_config: ProphyDatasource) {
+        this.fetchConfig = fetch_config
+    }
+
+    url_author(id: string) {
+        return `${this.fetchConfig.api_url}author/${id}`
+    }
+
+    url_api(json: any) {
+        const params = encodeQueryData(this.fetchConfig.api_params)
+        return json.paperId ? `${this.fetchConfig.api_url}paper/${json.paperId}?${params}` : ''
+    }
+
+    url_arxiv(json: any) {
+        return json.arxivId ? `https://arxiv.org/abs/${json.arxivId}` : ''
+    }
+
+    url_doi(json: any) {
+        return json.doi ? `https://doi.org/${json.doi}` : ''
+    }
+
+    searchline(doc: Paper) {
+        const auths = doc.authors.reduce((acc, au) => acc + au.name +  ' ', '')
+        const line = [doc.title, auths, doc.venue, doc.year].join(' ').toLowerCase()
+        return line + ' ' + unidecode(line)
+        // NOTE: check gunter
+    }
+
+    outbound_names(ref: Paper) {
+        const outs = [this.fetchConfig.shortname.toLocaleLowerCase()]
+        if (ref.url_arxiv) { outs.push('arxiv') }
+        if (ref.url_doi) { outs.push('doi') }
+        outs.push('scholar')
+        return outs
+    }
+
+    doc_authors(json: any): Author[] {
+        const toauth = (item) => {
+            const auth = new Author()
+            auth.name = item.name
+            auth.url = item.url
+            return auth
+        }
+        return json.authors.map(toauth)
+    }
+
+    reformat_document(json: any, index: number) {
+        const newdoc: Paper = new Paper(json.arxivId)
+
+        if (json.title) {
+            newdoc.title = remove_title_dates(json.title.trim())
+        } else {
+            newdoc.title = 'Unknown title'
+        }
+
+        newdoc.year = json.year
+        newdoc.venue = json.venue
+        newdoc.citation_count = json.citation_count
+        newdoc.url = json.url || null
+        newdoc.doi = json.doi
+        newdoc.arxivId = json.arxivId
+
+        newdoc.authors = this.doc_authors(json)
+        newdoc.api = this.url_api(json)
+        newdoc.url_arxiv = this.url_arxiv(json)
+        newdoc.url_doi = this.url_doi(json)
+
+        newdoc.paperId = json.paperId
+        newdoc.isInfluential = json.isInfluential || false
+
+        newdoc.simpletitle = remove_puctuation(newdoc.title).toLocaleLowerCase()
+        newdoc.searchline = this.searchline(newdoc)
+        newdoc.outbound = this.outbound_names(newdoc)
+        newdoc.index = index
+        return newdoc
+    }
+}
